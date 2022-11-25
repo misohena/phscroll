3e9a4047be4ef8110a7ef53eeee37a9589bd5d07
AKIYAMA Kouhei <misohena@gmail.com>
2022-11-25 14:18:52 +0900

    Change registration method to font-lock-keywords
    As it was necessary to register after org-modern.

--- org-phscroll.el
+++ org-phscroll.el
@@ -31,6 +31,8 @@
 (require 'org)
 (require 'phscroll)
 
+(defvar-local org-phscroll-font-lock-keywords (list '(org-phscroll--fontify)))
+
 (define-minor-mode org-phscroll-mode
   "Apply phscroll to org-table."
   :type 'boolean
@@ -38,11 +40,15 @@
   :group 'org-phscroll
   (if org-phscroll-mode
       (progn
+        ;; Turn on
         ;; Do not update on modification-hooks. Update on font-lock.
         (setq-local phscroll-update-area-display-on-modified nil)
+        (font-lock-add-keywords nil org-phscroll-font-lock-keywords)
         (phscroll-mode 1)
         (font-lock-flush))
+    ;; Turn off
     (setq-local phscroll-update-area-display-on-modified t)
+    (font-lock-remove-keywords nil org-phscroll-font-lock-keywords)
     (phscroll-delete-all) ;;@todo Keep manually added areas (Identify areas created by org-phscroll, and delete(remove-region) only areas created by org-phscroll when fontify)
     (phscroll-mode -1)))
 
@@ -112,13 +118,6 @@
             (if (< not-table-beg limit)
                 (phscroll-remove-region not-table-beg limit))))))
     nil))
-
-(defun org-phscroll--font-lock-set-keywords ()
-  ;; See: org-set-font-lock-defaults
-  (nconc
-   org-font-lock-extra-keywords
-   (list
-    '(org-phscroll--fontify))))
 
 
 ;;;; Support for table column shrink/expand
@@ -267,8 +266,6 @@
   (interactive)
   (add-hook 'org-mode-hook
             #'org-phscroll-mode)
-  (add-hook 'org-font-lock-set-keywords-hook
-            #'org-phscroll--font-lock-set-keywords)
   ;; for table shrink/expand
   (advice-add #'org-table--shrink-columns
               :after #'org-phscroll--table-shrink-columns)
@@ -294,8 +291,6 @@
   (interactive)
   (remove-hook 'org-mode-hook
                #'org-phscroll-mode)
-  (remove-hook 'org-font-lock-set-keywords-hook
-               #'org-phscroll--font-lock-set-keywords)
   ;; for table shrink/expand
   (advice-remove #'org-table--shrink-columns
                  #'org-phscroll--table-shrink-columns)
